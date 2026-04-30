4907b3048ad0186d2a618d252334c139a403ba31
no2101 <no2101@sina.com>
2026-05-01 07:24:03 +0800

    main.cpp大改

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
-#include "mainwindow.h"
-#include <QApplication>
+#include "mainwindow.h"    // 包含主窗口头文件
+#include <QApplication>     // 包含Qt应用程序类头文件
 
+// 程序入口函数
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    QApplication a(argc, argv);    // 创建Qt应用程序对象，处理命令行参数
+    MainWindow w;                  // 创建主窗口对象
+    w.show();                      // 显示主窗口
 
-    return a.exec();
+    return a.exec();               // 进入应用程序事件循环，等待用户操作
 }
